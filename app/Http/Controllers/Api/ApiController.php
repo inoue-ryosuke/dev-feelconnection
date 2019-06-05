900c9f9969138d470fd444835eac99ef05ca8264
ShuheiMiyazato <miyazato.shuhei@xchange.jp>
2019-06-05 19:36:11 +0900

    debugbarをコメントアウト

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -26,7 +26,7 @@ class ApiController extends Controller
 
     public function __destruct()
     {
-        \Debugbar::disable();
+//        \Debugbar::disable();
 //        logger('debugbar off');
     }
 
