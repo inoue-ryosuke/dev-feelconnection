29c8bfb7cd0ff50e20a2d9942d7b4b613876f822
Tan Nishitani <nishitani.tan@exchange.jp>
2019-06-11 14:41:45 +0900

    微修正

--- app/Libraries/Logic/ReservationModal/ReservationMasterResource.php
+++ app/Libraries/Logic/ReservationModal/ReservationMasterResource.php
@@ -21,67 +21,72 @@ abstract class ReservationMasterResource {
     protected $userMaster;
 
     /**
+     * Redisキャッシュで使用するカラム一覧
+     *
+     * shift_master:IDハッシュ値
+     * @var int shiftid 主キー
+     * @var string flg 有効フラグ(Y:有効、N:削除済み, C:休講)
+     * @var datetime open_datetime ネット予約公開日時
+     * @var int taiken_mess 体験制限表示(0:非表示, 1:表示)
+     * @var int taiken_les_flg 体験予約可(0:不可, 1:可)
+     * @var int tlimit tlimit分前まで予約可能
+     * @var date shift_date レッスン開催日
+     * @var time ls_st レッスン開始時間
+     * @var int shift_capa 定員(人数)
+     * @var int taiken_capa 体験定員(体験人数)
+     * @var int gender 性別制限(0:制限なし、1:女性のみ、2:男性のみ)
+     * @var int ls_menu レッスンID(lesson_master.lid)
+     * @var time ls_et レッスン終了時間
+     * @var int shift_tenpo_id 店舗ID(tenpo_master.tid)
+     * @var int teacher インストラクターID(user_master.uid)
+     *
+     * lesson_master:ID
+     * @var int lid 主キー
+     * @var int lesson_class1 lesson_class1.id
+     * @var int lesson_class2 lesson_class2.id
+     * @var int lesson_class3 lesson_class3.id
+     *
+     * lesson_class1:ID
+     * @var int id 主キー
+     * @var string name 分類名
+     *
+     * lesson_class2:ID
+     * @var int id 主キー
+     * @var string name 分類名
+     *
+     * lesson_class3:ID
+     * @var int id 主キー
+     * @var string name 分類名
+     *
+     * cust_master:ID
+     * @var int cid 主キー
+     * @var int memtype 会員種別(cust_memtype.mid)
+     *
+     * tenpo_master:ID
+     * @var int tid 主キー
+     * @var string tenpo_name 店舗名
+     *
+     * user_master:ID
+     * @var int uid 主キー
+     * @var string user_name インストラクター名
+     * @var string path_img インストラクター写真の画像パス
+     */
+
+    /**
      *
      * @param string $shiftIdHash shift_master.shiftid_hash
      */
     public function __construct(string $shiftIdHash) {
         $this->shiftIdHash = $shiftIdHash;
-        $this->shiftMaster = array(
-            'shiftid_hash' => $shiftIdHash, // shiftIdハッシュ値
-            'shiftid' => NULL, // 主キー
-            'flg' => NULL, // 有効フラグ(Y:有効、N:削除済み, C:休講)
-            'open_datetime' => NULL, // ネット予約公開日時
-            'taiken_mess' => NULL, // 体験制限表示(0:非表示, 1:表示)
-            'taiken_les_flg' => NULL, // 体験予約可(0:不可, 1:可)
-            'tlimit' => NULL, // tlimit分前まで予約可能
-            'shift_date' => NULL, // レッスン開催日
-            'ls_st' => NULL, // レッスン開始時間
-            'shift_capa' => NULL, // 定員(人数)
-            'taiken_capa' => NULL, // 体験定員(体験人数)
-            'gender' => NULL, // 性別制限,
-            'ls_menu' => NULL, // レッスンID(lesson_master.lid),
-            'ls_et' => NULL, // レッスン終了時間
-            'shift_tenpo_id' => NULL, // 店舗ID(tenpo_master.tid)
-            'teacher' => NULL, // インストラクターID(user_master.uid)
-        );
 
-        $this->lessonMaster = array(
-            'lid' => NULL, // 主キー
-            'lesson_class1' => NULL, // lesson_class1.id,
-            'lesson_class2' => NULL, // lesson_class2.id,
-            'lesson_class3' => NULL, // lesson_class3.id,
-        );
-
-        $this->lessonClass1 = array(
-            'id' => NULL, // 主キー,
-            'name' => NULL, // 分類名
-        );
-
-        $this->lessonClass2 = array(
-            'id' => NULL, // 主キー,
-            'name' => NULL, // 分類名
-        );
-
-        $this->lessonClass3 = array(
-            'id' => NULL, // 主キー,
-            'name' => NULL, // 分類名
-        );
-
-        $this->custMaster = array(
-            'cid' => NULL, // 主キー,
-            'memtype' => NULL, // 会員種別(cust_memtype.mid)
-        );
-
-        $this->tenpoMaster = array(
-            'tid' => NULL, // 主キー,
-            'tenpo_name' => NULL, // 店舗名
-        );
-
-        $this->userMaster = array(
-            'uid' => NULL, // 主キー,
-            'user_name' => NULL, // インストラクター名,
-            'path_img' => NULL, // インストラクター写真の画像パス
-        );
+        $this->shiftMaster = null;
+        $this->lessonMaster = null;
+        $this->lessonClass1 = null;
+        $this->lessonClass2 = null;
+        $this->lessonClass3 = null;
+        $this->custMaster = null;
+        $this->tenpoMaster = null;
+        $this->userMaster = null;
     }
 
     /**
@@ -103,16 +108,12 @@ abstract class ReservationMasterResource {
      * @return boolean 成功=true, 失敗=false
      */
     protected function createRedisResourceByKey(string $keyName, array &$resource) {
-        $cache = Redis::hgetall($keyName);
-        if (!empty($cache)) {
-            foreach ($cache as $key => $value) {
-                if (array_key_exists($key, $resource)) {
-                    $resource[$key] = $value;
-                }
-            }
-
+        $resource = Redis::hgetall($keyName);
+        if (!empty($resource)) {
             return true;
         } else {
+            $resource = null;
+
             return false;
         }
     }
