d514174a783a015df98a9f36fee71d03f7ce51d0
higuma.tadashi <higuma.tadashi@xchange.jp>
2019-06-12 18:08:56 +0900

    ・状況確認画面：個人情報表示APIスタブ実装 ・店舗名の修正（変更登録判定はまだ）

--- app/Libraries/Logic/Authentication/SelectLogic.php
+++ app/Libraries/Logic/Authentication/SelectLogic.php
@@ -45,4 +45,38 @@ class SelectLogic
         ];
         return $response;
     }
+
+    public static function getUserMoreInfo($cid) {
+        // TBD:認証情報からCustを特定する
+        if (!$cid) {
+            throw new BadRequestException(); // Hander経由レスポンスの場合
+        }
+        $cid = Cust::first()->cid;
+        $custinfo = Cust::getAuthInfo($cid);
+        if (!$custinfo) {
+            throw new BadRequestException(); // Hander経由レスポンスの場合
+        }
+        $response = [
+            "result_code" => 0,
+            "name" => $custinfo->getNameInfo(),
+            "kana" => $custinfo->kana,
+            "pc_mail" => $custinfo->pc_mail,
+            "b_birthday" => $custinfo->getBirthDayWord("jp"), //"昭和55年1月1日",
+            "b_year" => $custinfo->b_year,
+            "b_month" => $custinfo->b_month,
+            "b_day" => $custinfo->b_day,
+            "sex" => $custinfo->sex,
+            "h_zip" => $custinfo->h_zip,
+            "h_pref" => $custinfo->h_addr1,
+            "h_addr" => $custinfo->h_addr2,
+            "h_tel" => $custinfo->getTelNo(),
+            "memtype_name" => $custinfo->getMemTypeName(),  //"マンスリーメンバー",
+            "store_name" => $custinfo->getStoreNames(), //"銀座（GNZ）、自由が丘（JYO）",
+            "dm_list" => $custinfo->getDmLists(),       //"1,,,,",
+            "pc_conf" => $custinfo->getPcConf(),        // 1,
+            "campaign_list" => $custinfo->getCampaignList(),
+            "gmo_credit" => $custinfo->getGmoId()       // "XXXXXXXXXXX",
+        ];        
+        return $response;
+    }
 }
