baf19f53915249eb9db323dec0f65a78db8d201b
ShuheiMiyazato <miyazato.shuhei@xchange.jp>
2019-06-14 17:05:34 +0900

    ミドルウェアを追記

--- app/Http/Controllers/Api/MusicController.php
+++ app/Http/Controllers/Api/MusicController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 /**
- * 認証コントローラー
  * @Middleware({"api"})
  */
 class MusicController extends ApiController
