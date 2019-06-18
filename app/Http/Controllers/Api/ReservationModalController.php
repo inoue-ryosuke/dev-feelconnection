08010e77429649a22bfc6f29df74e39d1f7e5ff9
Tan Nishitani <nishitani.tan@exchange.jp>
2019-06-18 20:58:19 +0900

    メソッド整理、バリデーション追加

--- app/Http/Controllers/Api/ReservationModalController.php
+++ app/Http/Controllers/Api/ReservationModalController.php
@@ -201,6 +201,9 @@ class ReservationModalController extends Controller
         // 座席情報取得
         $sheetManager = new SheetManager($shiftMaster['shiftid']);
         $sheetManager->initStudio();
+        $sheetManager->setSheetStatusAndModalTypeByOrderLesson($custMaster['cid']);
+        $sheetManager->setSheetStatusAndModalTypeBySheetLock($custMaster['cid']);
+        $sheetManager->fillNotSpecialTrialSheet($custMaster['memtype']);
 
         // TODO 座席番号は、スタジオの座席数を超えた数値はエラー
         if (!$sheetManager->isSheetNoValid($sheet_no)) {
@@ -212,7 +215,7 @@ class ReservationModalController extends Controller
         }
 
         // 座席予約状態取得
-        $sheetStatus = $sheetManager->getSheetStatus($sheet_no, $custMaster['cid'], $custMaster['memtype']);
+        $sheetStatus = $sheetManager->getSheetStatus($sheet_no);
 
         return response()->json([
             'response_code' => Response::HTTP_OK,
@@ -418,6 +421,76 @@ class ReservationModalController extends Controller
                     array_merge($params, [ 'shift_master' => $shiftMaster ]))
                 );
             //throw new BadRequestException('未公開のレッスンです。');
+        }
+
+        // ネット・トライアル会員が体験予約不可のレッスンを指定した場合エラー
+        if (!VaidationLogic::canReserveByNetTrialMember($custMaster['memtype'], $shiftMaster['taiken_les_flg'])) {
+            // 体験予約不可のレッスンを指定
+            return CommonLogic::getErrorJsonResponse(
+                Response::HTTP_BAD_REQUEST,
+                CommonLogic::getErrorArray(
+                    'Invalid lesson',
+                    'ネット・トライアル会員が体験予約不可のレッスンを指定しました。',
+                    array_merge($params, [ 'cust_master' => $custMaster, 'shift_master' => $shiftMaster ]))
+                );
+            //throw new BadRequestException('ネット・トライアル会員が体験予約不可のレッスンを指定しました。');
+        }
+
+        // ログインユーザーの会員種別が、予約可能会員種別(tenpo_master.tenpo_memtype)かどうか。
+        if (!VaidationLogic::isMemberTypeReservable($futureMemberType, $tenpoMaster['tenpo_memtype'])) {
+            return CommonLogic::getErrorJsonResponse(
+                Response::HTTP_BAD_REQUEST,
+                CommonLogic::getErrorArray(
+                    'Invalid lesson',
+                    'レッスンの店舗を予約可能な会員種別ではありません。',
+                    array_merge($params, [ 'future_memtype' => $futureMemberType, 'tenpo_master' => $tenpoMaster ]))
+                );
+            //throw new BadRequestException('レッスンの店舗を予約可能な会員種別ではありません。');
+        }
+
+        // 体験レッスン未受講状態のバリデーション
+        $taikenResults = VaidationLogic::validateNotTakenTrialLesson($shiftMaster, $custMaster);
+        if (!$taikenResults['result']) {
+            return CommonLogic::getErrorJsonResponse(
+                Response::HTTP_BAD_REQUEST,
+                CommonLogic::getErrorArray(
+                    'Invalid lesson',
+                    $taikenResults['error_message'],
+                    array_merge($params, [ 'cust_master' => $custMaster, 'shift_master' => $shiftMaster]))
+                );
+            /*throw new BadRequestException('
+             体験予約不可のレッスンが指定されました。\n
+             予約済みの体験レッスンより後の日付を指定してください。
+             ');*/
+        }
+
+        // 座席情報取得
+        $sheetManager = new SheetManager($shiftMaster['shiftid']);
+        $sheetManager->initStudio();
+        $sheetManager->setSheetStatusAndModalTypeByOrderLesson($custMaster['cid']);
+        $sheetManager->setSheetStatusAndModalTypeBySheetLock($custMaster['cid']);
+        $sheetManager->fillNotSpecialTrialSheet($custMaster['memtype']);
+
+        // TODO 座席番号は、スタジオの座席数を超えた数値はエラー
+        if (!$sheetManager->isSheetNoValid($sheet_no)) {
+            return CommonLogic::getErrorJsonResponse(
+                Response::HTTP_BAD_REQUEST,
+                CommonLogic::getErrorArray('Invalid sheet_no', '無効な座席番号です、', $params)
+                );
+            //throw new BadRequestException('無効な座席番号です。');
+        }
+
+        // 予約受付時間内かどうか
+        if (!VaidationLogic::validateTimeLimit($shiftMaster['shift_date'], $shiftMaster['ls_st'], $shiftMaster['tlimit'])) {
+            // 予約受付時間外
+            return CommonLogic::getErrorJsonResponse(
+                Response::HTTP_CONFLICT,
+                CommonLogic::getErrorArray(
+                    'Can not reserve sheet',
+                    '予約受付時間外です。',
+                    array_merge($params, [ 'shift_master' => $shiftMaster ]))
+                );
+            //throw new ApplicationException('予約受付時間外です。', 409);
         }
 
         return response()->json([
