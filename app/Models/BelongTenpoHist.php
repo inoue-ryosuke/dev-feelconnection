c9e722acc2fc231bd96c99e271ab2a211835d09a
ShuheiMiyazato <miyazato.shuhei@xchange.jp>
2019-06-19 17:37:37 +0900

    インストラクター紹介API修正

--- app/Models/BelongTenpoHist.php
+++ app/Models/BelongTenpoHist.php
@@ -10,6 +10,14 @@ use App\Libraries\Auth\Authenticatable as AuthenticatableTrait;
 class BelongTenpoHist extends BaseModel implements Authenticatable
 {
     use AuthenticatableTrait;
+
+    const TABLE = 'belong_tenpo_hist__c';
+    const TID = 'tid__c';
+    const UID = 'uid__c';
+    const FLAG = 'flg__c';
+    const TABLE_TID = self::TABLE.'.'.self::TID;
+    const TABLE_UID = self::TABLE.'.'.self::UID;
+    const TABLE_FLAG = self::TABLE.'.'.self::FLAG;
     /**
      * @var string テーブル名
      */
